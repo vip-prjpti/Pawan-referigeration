7adade6684b2f8d54f7d7c2a90ddc682ac9e045d
vip-prjpti <prajapativipul0408@gmail.com>
2023-05-10 22:47:47 +0530

    first commit

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('#menu-btn');
+let navbarLinks = document.querySelector('.header .navbar .links');
+
+menu.onclick=() => {
+    menu.classList.toggle('fa-times');
+    navbarLinks.classList.toggle('active');
+}
+
